0d611894c129c92549e60456e0572e814b25c666
Andre Gama del Otero <andredelotero@gmail.com>
2022-01-02 15:51:14 -0300

    strict mode

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Rovers } from "./components/Rovers/Rovers";
@@ -7,25 +8,27 @@ import { StyledFooter } from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar>
-        <Link to={`/`} className="link">
-          Mars Photos - André Gama del Otero
-        </Link>
-      </Navbar>
+    <React.StrictMode>
+      <div className="App">
+        <Navbar>
+          <Link to={`/`} className="link">
+            Mars Photos - André Gama del Otero
+          </Link>
+        </Navbar>
 
-      <Routes>
-        <Route path="/" element={<Rovers />} />
-        <Route path="/roverdetail/:name" element={<RoverDetail />} />
-      </Routes>
-      <StyledFooter>
-        <p>
-          Mars Photos. A project by André Gama del Otero Created in 2022 with
-          React, Styled components and NASA open API "Mars Rover Photos"
-        </p>
-        <p>All photos belong to NASA</p>
-      </StyledFooter>
-    </div>
+        <Routes>
+          <Route path="/" element={<Rovers />} />
+          <Route path="/roverdetail/:name" element={<RoverDetail />} />
+        </Routes>
+        <StyledFooter>
+          <p>
+            Mars Photos. A project by André Gama del Otero Created in 2022 with
+            React, Styled components and NASA open API "Mars Rover Photos"
+          </p>
+          <p>All photos belong to NASA</p>
+        </StyledFooter>
+      </div>
+    </React.StrictMode>
   );
 }
 
